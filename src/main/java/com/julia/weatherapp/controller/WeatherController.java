bd678bf6b8446fc59424fdcf6f944c6ab7cebf47
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-14 21:27:44 -0500

    Remove unneeded imports and add model attribute

--- src/main/java/com/julia/weatherapp/controller/WeatherController.java
+++ src/main/java/com/julia/weatherapp/controller/WeatherController.java
@@ -1,12 +1,12 @@
 package com.julia.weatherapp.controller;
 
-import javax.validation.Valid;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.julia.weatherapp.model.Request;
@@ -16,22 +16,26 @@ import com.julia.weatherapp.service.WeatherService;
 
 @Controller
 public class WeatherController {
-    @Autowired
-    private WeatherService weatherService;
-    
-    @GetMapping
-    public String getIndex(Model model) {
-        model.addAttribute("request", new Request());  
-        model.addAttribute("zipCodes", weatherService.getMostRecentSearches());
-        return "index";
-    }
-    
-    @PostMapping
-    public String postIndex(Request request, Model model) {
-        Response data = weatherService.getForecast(request.getZipCode());
-        weatherService.addZipCode(request.getZipCode());
-        model.addAttribute("data", data);
-        return "index";
-    }
-    
+	@Autowired
+	private WeatherService weatherService;
+
+	@GetMapping
+	public String getIndex(Model model) {
+		model.addAttribute("request", new Request());
+		return "index";
+	}
+
+	@PostMapping
+	public String postIndex(Request request, Model model) {
+		Response data = weatherService.getForecast(request.getZipCode());
+		weatherService.addZipCode(request.getZipCode());
+		model.addAttribute("data", data);
+		return "index";
+	}
+	
+	@ModelAttribute (value = "zipCodes")
+	public List<ZipCode> recentSearches() {
+		return weatherService.getMostRecentSearches();
+	}
+
 }
